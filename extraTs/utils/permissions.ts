56af49469009e03db65402fad489ae26f3f92dd1
aman-successive <aman.raj@successive.tech>
2019-01-21 18:02:11 +0530

    doc:converted previous project files in typescript(#39520)

---
+++ extraTs/utils/permissions.ts
@@ -0,0 +1,14 @@
+import { permissions } from "../constants";
+export default function hasPermission(
+  moduleName: string,
+  permissionsType: string,
+  role: string
+): void {
+  if (permissions.hasOwnProperty(moduleName)) {
+    if (permissions[moduleName]["all"].includes(role)) {
+      console.log("true");
+    } else {
+      console.log(permissions[moduleName][permissionsType].includes(role));
+    }
+  }
+}
